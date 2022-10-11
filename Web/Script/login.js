91e65524c51df2ef550fe713c179033ca869094a
Gabriel De Moura <gabriel.demoura@smartbear.com>
2022-10-11 12:28:48 -0400

    changes to web proj

---
+++ Web/Script/login.js
@@ -0,0 +1,21 @@
+﻿function login()
+{
+  //Launches the specified browser and opens the specified URL in it.
+  Browsers.Item(btChrome).Run("https://bearstore-testsite.smartbear.com/");
+  //Maximizes the specified Window object.
+  Aliases.browser.BrowserWindow.Maximize();
+  //Clicks the 'textnodeLogIn' control.
+  Aliases.browser.pageShop.header.navUsd.linkLogIn.textnodeLogIn.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageLogin.Wait();
+  //Sets the text 'gabedemo' in the 'textboxUsernameoremail' text editor.
+  Aliases.browser.pageLogin.sectionContent.textboxUsernameoremail.SetText("gabedemo");
+  //Clicks the 'passwordboxPassword' control.
+  Aliases.browser.pageLogin.sectionContent.passwordboxPassword.Click();
+  //Sets the text Project.Variables.Password1 in the 'passwordboxPassword' text editor.
+  Aliases.browser.pageLogin.sectionContent.passwordboxPassword.SetText(Project.Variables.Password1);
+  //Clicks the 'buttonLogIn' button.
+  Aliases.browser.pageLogin.sectionContent.buttonLogIn.ClickButton();
+  //Checks whether the 'contentText' property of the Aliases.browser.pageShop.header.navUsd.linkGabedemo.textnodeGabedemo object equals 'gabedemo'.
+  aqObject.CheckProperty(Aliases.browser.pageShop.header.navUsd.linkGabedemo.textnodeGabedemo, "contentText", cmpEqual, "gabedemo");
+}
---
+++ Web/Script/zillowNamemapping.js
@@ -0,0 +1,15 @@
+﻿function refreshNamemapping()
+{
+  // Browsers.Item(btChrome).Run("https://www.zillow.com/homedetails/*");
+
+  var pnlProperty = Aliases.browser.pageSanFranciscoCaRealEstateSanF2.panel280m2;
+
+  // Do something that updates the web page and the pnlSpecialOffers object
+  // ...
+
+  // Force a search for the pnlSpecialOffers object
+  pnlProperty.RefreshMappingInfo();
+  // You can also force a search for all child mapped objects of pageExample
+  // Aliases.browser.pageExample.RefreshMappingInfo();
+
+}
