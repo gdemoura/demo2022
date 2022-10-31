e4536a9d21ff73d12681a12bc433104a17832ba4
Gabriel De Moura <gabriel.demoura@smartbear.com>
2022-10-31 15:00:46 -0400

    chnages to Web prj

---
+++ Web/Script/GetSubString1.js
@@ -0,0 +1,7 @@
+﻿function GetSubString1()
+{
+  var first = Aliases.browser.pageBallChair.sectionContent.articleBallChair.asideBallChair.textnode259900ExclTax.contentText;
+  var firstPrice = aqString.SubString(first, 0, 9);
+  Project.Variables.firstPrice = firstPrice;
+
+}
---
+++ Web/Script/GetSubString3.js
@@ -0,0 +1,8 @@
+﻿function GetSubString3()
+{
+  var firstPrice = Aliases.browser.pageBallChair.sectionContent.articleBallChair.asideBallChair.textnode259900ExclTax.contentText;
+  
+  // Uses the SubString method
+  Log.Message( aqString.SubString(thirdPrice, 0, 9) ); 
+
+}
