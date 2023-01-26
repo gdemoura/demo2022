c5f59939b79d9d1a2163f5d588fe6071a57ac5e7
Gabriel De Moura <gabriel.demoura@smartbear.com>
2023-01-26 09:52:07 -0500

    chnages to web project

--- Web/Script/GetSubString1.js
+++ Web/Script/GetSubString1.js
@@ -3,5 +3,23 @@
   var first = Aliases.browser.pageBallChair.sectionContent.articleBallChair.asideBallChair.textnode259900ExclTax.contentText;
   var firstPrice = aqString.SubString(first, 0, 9);
   Project.Variables.firstPrice = firstPrice;
+  Browsers.Item(btChrome).Navigate("https://bearstore-testsite.smartbear.com/");
+  let browser = Aliases.browser;
+  browser.BrowserWindow.Maximize();
+  browser.pageShop.header.navUsd.linkLogIn.textnodeLogIn.Click();
+  let page = browser.pageLogin;
+  page.Wait();
+  let textbox = page.sectionContent;
+  let textbox2 = textbox.textboxUsernameoremail;
+  textbox2.Click();
+  textbox2.SetText("gabedemo");
+  let passwordBox = textbox.Password;
+  passwordBox.Click();
+  passwordBox.SetText(Project.Variables.Password1);
+  textbox.buttonLogIn.ClickButton();
+}
 
+function EventControl1_OnStartTest(Sender)
+{
+  
 }
