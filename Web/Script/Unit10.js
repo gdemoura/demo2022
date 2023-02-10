ff73f614577dab81dd98009e757ce4eb4162e633
Gabriel De Moura <gabriel.demoura@smartbear.com>
2023-02-10 10:51:05 -0500

    changes to .pjs

---
+++ Web/Script/Unit10.js
@@ -0,0 +1,5 @@
+﻿function Test1()
+{
+  //Compares the imageShowProductsInCategory Stores item with the image of the Aliases.browser.pageShop.sectionContent.articleFurniture.linkShowProductsInCategory.imageShowProductsInCategory object.
+  Regions.imageShowProductsInCategory.Check(Aliases.browser.pageShop.sectionContent.articleFurniture.linkShowProductsInCategory.imageShowProductsInCategory, false, false, 1708, 0, "imageShowProductsInCategory_Mask");
+}
